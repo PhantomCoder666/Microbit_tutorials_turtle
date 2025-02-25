9d7099f6da25c24ae9d0e90da3cd68c67b211014
PhantomCoder666 <lachlan674762@brisbanegrammar.com>
2025-02-26 05:52:17 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.pen(TurtlePenMode.Down)
+})
